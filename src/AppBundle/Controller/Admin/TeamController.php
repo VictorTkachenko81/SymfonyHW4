e70aeba090ff0b76dd4f944a4c811af96ddd6cd2
Victor Tkachenko <tkachenko.victor.ukr@gmail.com>
2015-12-25 14:56:47 +0200

    added adminTeamController

---
+++ src/AppBundle/Controller/Admin/TeamController.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @link http://webmozart.io
+ * pattern fabrica
+ */
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Team;
+use AppBundle\Form\Type\TeamType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * @Route("/admin")
+ */
+class TeamController extends Controller
+{
+     /**
+     * @Route("/team", name="adminTeam")
+     * @Template("AppBundle:admin:team.html.twig")
+     * @Method("GET")
+     *
+     * @return Response
+     */
+    public function teamAction()
+    {
+        $teams = $this->getDoctrine()
+            ->getRepository('AppBundle:Team')
+            ->findAll();
+
+        return [
+            'teams' => $teams,
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/team/new", name="teamNew")
+     * @Method({"GET", "POST"})
+     * @Template("AppBundle:admin/form:team.html.twig")
+     *
+     * @return Response
+     */
+    public function newTeamAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $team = new Team();
+
+        $form = $this->createForm(TeamType::class, $team, [
+            'em' => $em,
+            'action' => $this->generateUrl('teamNew'),
+            'method' => Request::METHOD_POST,
+        ]);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em->persist($team);
+                $em->flush();
+
+                return $this->redirectToRoute('adminTeam');
+            }
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+
+    /**
+     * @param $teamCode
+     * @param Request $request
+     * @Route("/team/{teamCode}/edit", name="teamEdit", requirements={
+     *     "teamCode": "[A-Za-z]+"
+     *     })
+     * @Method({"GET", "POST"})
+     * @Template("AppBundle:admin/form:team.html.twig")
+     *
+     * @return Response
+     */
+    public function editTeamAction($teamCode, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $team = $em->getRepository('AppBundle:Team')
+            ->findOneByCode($teamCode);
+
+        $form = $this->createForm(TeamType::class, $team, [
+            'em' => $em,
+            'action' => $this->generateUrl('teamEdit', ['teamCode' => $teamCode]),
+            'method' => Request::METHOD_POST,
+        ]);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em->flush();
+
+                return $this->redirectToRoute('adminTeam');
+            }
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+
+    /**
+     * @param $teamCode
+     * @Route("/team/{teamCode}/remove", name="teamRemove", requirements={
+     *     "teamCode": "[A-Za-z]+"
+     *     })
+     * @Method("GET")
+     * @Template("AppBundle:admin/form:team.html.twig")
+     *
+     * @return Response
+     */
+    public function removeTeamAction($teamCode)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $team = $em->getRepository('AppBundle:Team')
+            ->findOneByCode($teamCode);
+
+        $em->remove($team);
+        $em->flush();
+
+        return $this->redirectToRoute('adminTeam');
+    }
+
+}
---
+++ src/AppBundle/Form/Type/TeamType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('code', TextType::class)
+            ->add('country', EntityType::class, array(
+                'class' => 'AppBundle:Country',
+                'choice_label' => 'name',
+            ))
+            ->add('logo', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Save'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Team',
+            'em' => null,
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return "team";
+    }
+}
